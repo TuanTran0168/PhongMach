d3f0a44167ca1c9dfc36db2272f18219df12c41e
TuanTran0168 <82080825+TuanTran0168@users.noreply.github.com>
2022-12-11 17:55:41 +0700

    Update models.py
    update gender, birthday for user

--- saleappv1/saleapp/models.py
+++ saleappv1/saleapp/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, Float, ForeignKey, Enum, DateTime
+from sqlalchemy import Column, Integer, String, Text, Boolean, Float, ForeignKey, Enum, DateTime, Date
 from sqlalchemy.orm import relationship, backref
 from saleapp import db, app
 from enum import Enum as UserEnum
@@ -27,12 +27,13 @@ class ListUser(BaseModel):
 
 class User(BaseModel, UserMixin):
     name = Column(String(50), nullable=False)
+    birthday = Column(Date, nullable=False)
+    gender = Column(Boolean, default=True, nullable=False)
     username = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
     avatar = Column(String(200), nullable=False)
     active = Column(Boolean, default=True)
     user_role = Column(Enum(UserRole), default=UserRole.USER)
-    # receipts = relationship('Receipt', backref='user', lazy=True)
     list_user_id = Column(Integer, ForeignKey(ListUser.id), nullable=False)
     medical_report = relationship("MedicalReport", backref='user', lazy=True)
     receipt = relationship('Receipt', backref='user', lazy=True)
@@ -46,10 +47,12 @@ class Receipt(BaseModel):
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
     price = Column(Float, default=0)
 
+
 class DiseaseList(BaseModel):
     name = Column(String(50), nullable=False)
     # disease_id = Column(Integer, ForeignKey(Disease.id), nullable=False)
-    disease = relationship("Disease", backref = "DiseaseList", lazy = True)
+    disease = relationship("Disease", backref="DiseaseList", lazy=True)
+
 
 class Disease(BaseModel):
     name = Column(String(50), nullable=False)
@@ -96,27 +99,9 @@ class MedicalReportDetail(BaseModel):
     medical_report_id = Column(Integer, ForeignKey(MedicalReport.id), nullable=False)
 
 
-# class Tag(BaseModel):
-#     name = Column(String(50), nullable=False, unique=True)
-#
-#     def __str__(self):
-#         return self.name
-
-
-# class Receipt(BaseModel):
-#     created_date = Column(DateTime, default=datetime.now())
-#     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-#     details = relationship('ReceiptDetails', backref='receipt', lazy=True)
-
-
-# class ReceiptDetails(BaseModel):
-#     quantity = Column(Integer, default=0)
-#     price = Column(Float, default=0)
-#     product_id = Column(Integer, ForeignKey(Product.id), nullable=False)
-#     receipt_id = Column(Integer, ForeignKey(Receipt.id), nullable=F
 if __name__ == '__main__':
     with app.app_context():
-        # db.create_all()
+        db.create_all()
 
         import hashlib
 
@@ -124,37 +109,37 @@ if __name__ == '__main__':
 
         l1 = ListUser(name="Danh sách Admin")
         l2 = ListUser(name="Danh sách nhân viên")
-        l3 = ListUser(name = "Danh sách bệnh nhân")
+        l3 = ListUser(name="Danh sách bệnh nhân")
 
-        u1 = User(name="Tuấn", username="admin", password=password,
-                  avatar="http://it.ou.edu.vn/asset/ckfinder/userfiles/5/images/giang_vien/Vinh_2.jpg", active = True,
+        u1 = User(name="Tuấn", birthday=datetime.now(), gender=True, username="admin", password=password,
+                  avatar="http://it.ou.edu.vn/asset/ckfinder/userfiles/5/images/giang_vien/Vinh_2.jpg", active=True,
                   user_role=UserRole.ADMIN, list_user_id=1)
-        u2 = User(name="Thái", username="admin", password=password,
+        u2 = User(name="Thái", birthday=datetime.now(), gender=True, username="admin", password=password,
                   avatar="http://it.ou.edu.vn/asset/ckfinder/userfiles/5/images/giang_vien/Vinh_2.jpg", active=True,
                   user_role=UserRole.NURSE, list_user_id=2)
-        u3 = User(name="Trang", username="admin", password=password,
+        u3 = User(name="Trang", birthday=datetime.now(), gender=False, username="admin", password=password,
                   avatar="http://it.ou.edu.vn/asset/ckfinder/userfiles/5/images/giang_vien/Vinh_2.jpg", active=True,
                   user_role=UserRole.DOCTOR, list_user_id=2)
-        u4 = User(name="Hùng", username="admin", password=password,
+        u4 = User(name="Hùng", birthday=datetime.now(), gender=True, username="admin", password=password,
                   avatar="http://it.ou.edu.vn/asset/ckfinder/userfiles/5/images/giang_vien/Vinh_2.jpg", active=True,
                   user_role=UserRole.CASHIER, list_user_id=2)
-        u5 = User(name="Thành", username="admin", password=password,
+        u5 = User(name="Thành", birthday=datetime.now(), gender=True, username="admin", password=password,
                   avatar="http://it.ou.edu.vn/asset/ckfinder/userfiles/5/images/giang_vien/Vinh_2.jpg", active=True,
                   user_role=UserRole.USER, list_user_id=3)
 
-        ld1 = DiseaseList(name = "Tim")
+        ld1 = DiseaseList(name="Tim")
         ld2 = DiseaseList(name="Mạch")
 
-        d1 = Disease(name = "Thiếu máu cơ tim", disease_list_id = 1)
-        d2 = Disease(name = "Bệnh viêm cơ tim", disease_list_id = 1)
-        d3 = Disease(name = "Bệnh mạch vành", disease_list_id = 2)
-        d4 = Disease(name = "Bệnh động mạch ngoại biên", disease_list_id = 2)
+        d1 = Disease(name="Thiếu máu cơ tim", disease_list_id=1)
+        d2 = Disease(name="Bệnh viêm cơ tim", disease_list_id=1)
+        d3 = Disease(name="Bệnh mạch vành", disease_list_id=2)
+        d4 = Disease(name="Bệnh động mạch ngoại biên", disease_list_id=2)
 
         c1 = Category(name="Thuốc thể rắn")
         c2 = Category(name="Thuốc thể mềm")
         c3 = Category(name="Thuốc thể lỏng")
 
-        p1 = Product(name = "Thuốc A", unit = "viên", description = "Uống", price = 100000, active = 1, category_id = 1)
+        p1 = Product(name="Thuốc A", unit="viên", description="Uống", price=100000, active=1, category_id=1)
         p2 = Product(name="Thuốc B", unit="viên", description="Ngậm", price=30000, active=1, category_id=1)
         p3 = Product(name="Thuốc C", unit="gói", description="Uống", price=150000, active=1, category_id=3)
         p4 = Product(name="Thuốc D", unit="bịch", description="Nhai", price=200000, active=1, category_id=2)
